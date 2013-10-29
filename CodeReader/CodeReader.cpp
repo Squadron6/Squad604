19a130f6b4fc4e97e8ed6793b9a98c9686f702da
vlouie <v.louie91@gmail.com>
2013-10-29 12:41:13 -0700

    Updating CodeReader, compile with: g++ CodeReader.cpp

--- CodeReader/CodeReader.cpp
+++ CodeReader/CodeReader.cpp
@@ -4,11 +4,15 @@ Code reader
 For every .cpp file that exists in codebase
 read each line of code and store function calls for colour and link function
 */
-
+#include <iostream>
+#include <stdio.h>
 #include <dirent.h>
+
+using namespace std;
 
 int main(void){
   DIR *dp;
+  struct dirent *entry;
   const char *dirname = "."; //path of directory to read
   dp = opendir(dirname);
   if (dp){
