1d68af0b7e6f453b9d27f17bdc32459c7ccda13e
vlouie <v.louie91@gmail.com>
2013-11-05 23:53:09 -0800

    Refactoring CodeInjector code, not tested, but it does compile. Also added comments

--- CodeReader/CodeInjector.cpp
+++ CodeReader/CodeInjector.cpp
@@ -1,8 +1,10 @@
 /*
 Code Injector
 
-For every .cpp file that exists in codebase
-read each line of code and store function calls for colour and link function
+The Code Injector goes through all the directories in a codebase
+and injects a log statement into each .cpp file such that when
+the codebase is compiled and run to catch dynamic input, a log
+file is created documenting which methods are being called.
 */
 #include <iostream>
 #include <stdio.h>
@@ -12,6 +14,11 @@ read each line of code and store function calls for colour and link function
 #include <cstring>
 using namespace std;
 
+// inject_code takes in a file name and directory name
+// both as strings.
+// It uses these to read in the .cpp file and then does
+// a check for each line to see if a log statement
+// should be injected.
 void inject_code(string fileName, string dirName)
 {
 	ifstream infile;
@@ -55,7 +62,12 @@ void inject_code(string fileName, string dirName)
 	rename(tempNamec, fulName);
 }
 
-void exloreDirectory(string directory){
+// exploreDirectory takes in the path to the codebase directory as a string
+// and explores this directory, looking for every .cpp file in order to
+// inject log statements into it.
+// When there are sub-directories, exploreDirectory explores these as well
+// using recursion.
+void exploreDirectory(string directory){
   unsigned char isFile = 0x8;
   unsigned char isFolder = 0x4;
   DIR *dp;
@@ -63,63 +75,24 @@ void exloreDirectory(string directory){
   const char *dirname = directory.c_str(); //path of directory to read
   dp = opendir(dirname);
 
-  ifstream testfile;
-  string thing;
-
   if (dp){
     while (entry = readdir(dp)){ // for every entry in the directory
       string str(entry->d_name);
       if (entry->d_type == isFile && str.compare(str.size()-3, 3, "cpp") == 0){ // if it is a cpp file
-        cout << entry->d_name << endl; // print out the name of the file
-        testfile.open(entry->d_name);
-        getline(testfile, thing); //just going to dump out the first line for testing
-        cout << thing << endl;
-        testfile.close();
+      	string fileName = entry->d_name;
+        string dirNames = dirname;
+        string fullName = dirNames+"/"+ fileName;
+        inject_code(fileName, dirNames);
       }
       else if (entry->d_type == isFolder && str!="." && str!=".."){ // if it's a folder, not current or parent
-        //exploreDirectory(str); //how can this be recursive? it says exploreDirectory was not declared in this scope... do we need to create a .h file?
+        exploreDirectory(str);
       }
     }
   }
 }
 
 int main(void){
-  /**** NOTE START
-        integrate the following into the pre-existing code injection stuff 
-        based on the directory path, the following code will go through all the .cpp files 
-        TODO: traverse subdirectories & look for cpp files recursively; pull this out into a separate method? ****/
-  unsigned char isFile = 0x8;
-  unsigned char isFolder = 0x4;
-  DIR *dp;
-  struct dirent *entry;
-  const char *dirname = "../codeBase/fish-shell-master"; //path of directory to read
-  dp = opendir(dirname);
-  
-  /* the next 2 lines are to-be-removed */
-  ifstream testfile;
-  string thing;
-
-  if (dp){
-    while (entry = readdir(dp)){ // for every entry in the directory
-      string str(entry->d_name);
-      if (entry->d_type == isFile && str.compare(str.size()-3, 3, "cpp") == 0){ // if it is a cpp file
-      	string fileName = entry->d_name;
-	string dirNames = dirname;
-	string fullName = dirNames+"/"+ fileName;
-	inject_code(fileName, dirNames);
-      }
-      else if (entry->d_type == isFolder && str!="." && str!=".."){ // if it's a folder, not current or parent
-        // we need to explore this folder too, checking for .cpp files
-        // this likely means that this funtionality needs to pulled out into a function like...
-        // void directoryExplorer(string directoryPath)
-        //  use the code from the above note
-        //  and for the folder case (this condition!) then recursively call this method with new folder as path
-        // not sure if folder path needs ./ to be prepended
-        cout << "this is a folder! " << entry->d_name << endl;
-      }
-    }
-  /****** NOTE END ******/
-  // I think that the following needs to be moved into exploreDirectory into the file case
- }
+  string dirname = "../codeBase/fish-shell-master"; //path to codebase
+  exploreDirectory(dirname);
 }
 
