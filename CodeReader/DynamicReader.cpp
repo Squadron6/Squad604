64ccc0f2fc4826a21dd84a8e14a1bc46d759a29c
gezed <george__zheng@msn.com>
2013-11-14 11:37:26 -0800

    adding log parsing feature to DynamicReader.cpp
    DynamicReader now parses the log made by functions added by the code injector

--- CodeReader/DynamicReader.cpp
+++ CodeReader/DynamicReader.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include <stdio.h>
-#include <dirent.h>
 #include <fstream>
 #include <string>
-#include "CodeReader.h"
+#include <stack>
+#include <cstring>
+using namespace std;
 
+void parse_log(string logfile)
+{	
+	std::stack<string> functionStack;
+	ifstream infile;
+	ofstream outfile;
+	const char * fname = logfile.c_str();
+	string currline;
+	infile.open(fname);
+	outfile.open("parsedLog.log");
+	while(!infile.eof())
+	{
+	getline(infile, currline);
+	if(currline.compare("return") != 0)
+		{
+		functionStack.push(currline);
+		}
+	else
+		{
+		string toFunc = functionStack.top();
+		functionStack.pop();
+		string fromFunc = functionStack.top();
+		outfile << fromFunc << " " << toFunc << "\n";
+		}
+	}
+	infile.close();
+	outfile.close();
+	
+}
+
+int main(void)
+{
+	string peek;
+	std::stack<string> funcStack;
+	funcStack.push("test");
+	funcStack.push("test2");
+	peek = funcStack.top();
+	cout<<endl<<"peeked at: "<<peek<<endl;
+	parse_log("test.log");
+}
 
