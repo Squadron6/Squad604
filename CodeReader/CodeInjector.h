c5b955c3b9cc883ed755de5ca23c34f3b3eb4316
Don Lee <dlpowered89@gmail.com>
2013-11-14 15:04:31 -0800

    Updating header files

--- CodeReader/CodeInjector.h
+++ CodeReader/CodeInjector.h
@@ -2,6 +2,3 @@
 //
 //For every .cpp file that exists in codebase
 //read each line of code and store function calls for colour and link function
-
-#include "Colourizer.h"
-
--- CodeReader/Colourizer.h
+++ CodeReader/Colourizer.h
@@ -1,14 +1,11 @@
 //Colourizer header file
-//
-//include
-//include
 
 #include <unordered_map>
 
 #ifndef Colourizer_h
 #define Colourizer_h
 
-
-
+int find_max(unordered_map);
+unordered_map convert_to_RGB(unordered_map)
 
 #endif
