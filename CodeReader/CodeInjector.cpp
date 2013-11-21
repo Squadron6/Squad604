2c7e50602c471977b4025b1c2be9e5e50caa1f70
vlouie <v.louie91@gmail.com>
2013-11-20 16:56:43 -0800

    Reverted CodeInjector back to before return case was added, otherwise Fish doesn't run and throws errors. Fish compiles, installs and works with this commit

--- CodeReader/CodeInjector.cpp
+++ CodeReader/CodeInjector.cpp
@@ -6,17 +6,15 @@ and injects a log statement into each .cpp file such that when
 the codebase is compiled and run to catch dynamic input, a log
 file is created documenting which methods are being called.
 */
-
 #include <iostream>
 #include <stdio.h>
 #include <dirent.h>
 #include <fstream>
 #include <string>
 #include <cstring>
-using namespace std;
 #include "CodeInjector.h"
 
-
+using namespace std;
 
 // inject_code takes in a file name and directory name
 // both as strings.
@@ -25,115 +23,76 @@ using namespace std;
 // should be injected.
 void inject_code(string fileName, string dirName)
 {
-	ifstream infile;
-	string currLine;
-	int lineCOunt = 1;
-	const char * fname = fileName.c_str();
-	const char * dname = dirName.c_str();
-	string fullName = dirName + "/" + fileName;
-	const char * fulName = fullName.c_str();
-	string tempName = dirName+"/"+"temp.cpp";
-	const char * tempNamec = tempName.c_str();
-	string oldName = dirName+"/"+fileName+".old";
-	const char * oldNamec = oldName.c_str();
+        ifstream infile;
+        string currLine;
+        int lineCOunt = 1;
+        const char * fname = fileName.c_str();
+        const char * dname = dirName.c_str();
+        string fullName = dirName + "/" + fileName;
+        const char * fulName = fullName.c_str();
+        string tempName = dirName+"/"+"temp.cpp";
+        const char * tempNamec = tempName.c_str();
+        string oldName = dirName+"/"+fileName+".old";
+        const char * oldNamec = oldName.c_str();
   bool flag = false; //use to flag cases where we won't inject
-  bool returnflag = false;
-  bool ifflag = false;
   string flagcase1 = "class";
   string flagcase2 = "struct";
   string flagcase3 = "enum";
   string flagcase4 = "static const struct";
-  string flagcase6 = "static const";
+  string flagcase5 = "static const";
+  string flagcase6 = "const";
   string flagcase7 = "typedef struct";
   string flagcase8 = "static enum";
   string flagcase9 = "extern \"C\" {";
   string flagcase10 = "static struct";
   string flagcase11 = "typedef enum";
-  string flagcase12 = "static size_t hack_iconv";
-	
-	infile.open(fulName);
-	ofstream tempfile;
-	tempfile.open(tempNamec);
-	tempfile << "#include <fstream>" << endl;
-	while(!infile.eof())
-	{
+        
+        infile.open(fulName);
+        ofstream tempfile;
+        tempfile.open(tempNamec);
+        tempfile << "#include <fstream>" << endl;
+        while(!infile.eof())
+        {
     getline(infile, currLine);
-    if((!currLine.compare(0, flagcase1.size(), flagcase1) || !currLine.compare(0, flagcase2.size(), flagcase2) || 
+    if((!currLine.compare(0, flagcase1.size(), flagcase1) || !currLine.compare(0, flagcase2.size(), flagcase2) ||
     !currLine.compare(0, flagcase3.size(), flagcase3) || !currLine.compare(0, flagcase4.size(), flagcase4) ||
-    (!currLine.compare(0, flagcase6.size(), flagcase6) && currLine.find("=") != std::string::npos)||
+    !currLine.compare(0, flagcase5.size(), flagcase5) || !currLine.compare(0, flagcase6.size(), flagcase6) ||
     !currLine.compare(0, flagcase7.size(), flagcase7) || !currLine.compare(0, flagcase8.size(), flagcase8) ||
     !currLine.compare(0, flagcase9.size(), flagcase9) || !currLine.compare(0, flagcase10.size(), flagcase10) ||
-    !currLine.compare(0, flagcase11.size(), flagcase11) || !currLine.compare(0, flagcase12.size(), flagcase12))
-    && currLine.compare(currLine.size()-1, 1, ";")) 
+    !currLine.compare(0, flagcase11.size(), flagcase11))
+    && currLine.compare(currLine.size()-1, 1, ";"))
     {
       flag = true;
-    }
-    if(currLine.find("if (") != std::string::npos)
-    {
-      ifflag = true;
+      //printf("%s\n", currLine.c_str());
     }
     if(currLine.compare("{") != 0 && currLine.compare("}") != 0)
     {
-      if(currLine.find("return") != std::string::npos && currLine.find(";") != std::string::npos 
-      && currLine.find("printf") == std::string::npos && currLine.find("err(") == std::string::npos 
-      && currLine.find("/") == std::string::npos) 
-      {
-        //printf("%s\n", currLine.c_str());
-        returnflag = true;
-        if (ifflag && !flag){
-        currLine = "{\nlogfile.open(\"log.txt\");\nlogfile << \"return\" << std::endl;\nlogfile.close();\n" + currLine + "}\n";
-        tempfile << currLine << "\n";
-        ifflag = false;
-        }
-        else if (!ifflag && !flag){
-        currLine = "logfile.open(\"log.txt\");\nlogfile << \"return\" << std::endl;\nlogfile.close();\n" + currLine;
-        tempfile << currLine << "\n";
-        }
-        else{
-        tempfile << currLine << "\n";
-        }
-      }
-      else{
-        if(currLine.find("{") != std::string::npos){
-          ifflag = false; 
-          tempfile << currLine << "\n";
-        }
-        else{
-          tempfile << currLine << "\n";
-        }
-      }
+    tempfile << currLine << "\n";
     }
     else if(currLine.compare("{") == 0 && !flag)
     {
-      currLine = currLine + "std::ofstream logfile;\nlogfile.open(\"log.txt\");\nlogfile << __func__ << std::endl;\nlogfile.close();\n";
-      tempfile << currLine << "\n";
-      ifflag = false;
+    currLine = currLine + "std::ofstream logfile;\nlogfile.open(\"log.txt\", std::ios::app);\nlogfile << __func__ << std::endl;\nlogfile.close();\n";
+    tempfile << currLine << "\n";
     }
     else if(currLine.compare("{") == 0 && flag)
     {
-      ifflag = false;
-      tempfile << currLine << "\n";
+    tempfile << currLine << "\n";
     }
     else if(currLine.compare(0, 1, "}") == 0 && !flag)
     {
-      currLine = "logfile.open(\"log.txt\");\nlogfile << \"return\" << std::endl;\nlogfile.close();\n" + currLine;
-      tempfile << currLine << "\n";
+          currLine = "logfile.open(\"log.txt\", std::ios::app);\nlogfile << \"return\" << std::endl;\nlogfile.close();\n" + currLine;
+          tempfile << currLine << "\n";
     }
     else if(currLine.compare(0, 1, "}") == 0 && flag)
     {
-      tempfile << currLine << "\n";
-      flag = false;
-      returnflag = false;
+    tempfile << currLine << "\n";
+    flag = false;
     }
-    else{
-      flag = false;
-      tempfile << currLine << "\n";
-    }
-	}
-	tempfile.close();
-	infile.close();
-	rename(fulName, oldNamec);
-	rename(tempNamec, fulName);
+        }
+        tempfile.close();
+        infile.close();
+        rename(fulName, oldNamec);
+        rename(tempNamec, fulName);
 }
 
 // exploreDirectory takes in the path to the codebase directory as a string
@@ -153,7 +112,7 @@ void exploreDirectory(string directory){
     while (entry = readdir(dp)){ // for every entry in the directory
       string str(entry->d_name);
       if (entry->d_type == isFile && str.compare(str.size()-3, 3, "cpp") == 0){ // if it is a cpp file
-      	string fileName = entry->d_name;
+              string fileName = entry->d_name;
         string dirNames = dirname;
         string fullName = dirNames+"/"+ fileName;
         inject_code(fileName, dirNames);
@@ -170,4 +129,5 @@ int main(void){
   //string dirname = "../codeBase/fish-shell-master"; //path to codebase
   exploreDirectory(dirname);
 }
+
 
