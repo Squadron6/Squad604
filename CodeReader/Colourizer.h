b40eddad7c342525cf4adfc9ea8dae1bfcb0981b
sprakash0 <sonika.prakash@live.com>
2013-11-21 00:15:10 -0800

    Added two missing arguments to convert_to_RGB

--- CodeReader/Colourizer.h
+++ CodeReader/Colourizer.h
@@ -8,5 +8,5 @@
 static int find_max(std::unordered_map<std::string, int> input);
 static int find_min(std::unordered_map<std::string, int> input);
 
-static std::unordered_map<std::string, std::string> convert_to_RGB(std::unordered_map<std::string, int> input);
+static std::unordered_map<std::string, std::string> convert_to_RGB(std::unordered_map<std::string, int> input, int max, int min);
 #endif
