f61eb61ba6bb53a02fd5ca1e4b2379ea7e361f4c
gezed <george__zheng@msn.com>
2013-11-21 16:26:02 -0800

    fixed more

--- Pisces/Colourizer.h
+++ Pisces/Colourizer.h
@@ -8,5 +8,5 @@
 int find_max(std::unordered_map<std::string, int> input);
 int find_min(std::unordered_map<std::string, int> input);
 
-static std::unordered_map<std::string, std::string> convert_to_RGB(std::unordered_map<std::string, int> input, int max, int min);
+std::unordered_map<std::string, std::string> convert_to_RGB(std::unordered_map<std::string, int> input, int max, int min);
 #endif
