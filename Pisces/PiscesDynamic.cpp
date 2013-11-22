da636920434f395d6586ba3bc0e2183f3e0c3f69
vlouie <v.louie91@gmail.com>
2013-11-21 19:11:56 -0800

    Dynamic analysis essentially works: compile with make, then run ./dynamic, make sure you have the codebase directory set in PiscesDynamic.cpp and all the dependencies to run Fish, fish will open & I don't know how to exit it automatically yet so just type exit and the DynamicReader stuff will run, though I got a SegFault. Still a work in progress...

--- Pisces/PiscesDynamic.cpp
+++ Pisces/PiscesDynamic.cpp
@@ -11,30 +11,22 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-  string dirname = "../../fish-shell-master"; //path to codebase
-  //string logname = "../../fish-shell-master/log.txt"; 
+  string dirname = "../../fish-shell-master"; //path to codebase!!!!! TODO: update this? I'm leaving it like this for testing, otherwise you have to delete and re-copy it over every time due to the injection
   string logname = "log.txt"; 
-  string fishnav = "cd ../../fish-shell-master";
-  string fishauto = "(cd ../../fish-shell-master && autoconf)";
+  string fishauto = "(cd ../../fish-shell-master && autoconf)"; // TODO: use dirname var instead?
   string fishconf = "(cd ../../fish-shell-master && ./configure --prefix=$HOME)";
   string fishmake = "make install -C ../../fish-shell-master";
-  string echotest = "pwd";
   string fishrun = "fish";
-  string cmd = "ls";
-  string cmd2 = "exit";
-  //string fishmake = "(cd ../../fish-shell-master && make install)";
 
-  system(fishnav.c_str());
+  exploreDirectory(dirname);
   system(fishauto.c_str());
   system(fishconf.c_str());
   system(fishmake.c_str());
 
-  exploreDirectory(dirname);
   system(echotest.c_str());
   system(cmd2.c_str());
   system(fishrun.c_str());
-  system(cmd.c_str());
-  system(cmd2.c_str());
+  // Fish then opens and you have to give it input at this point
 
 	unordered_map<string, int> functionMap;
 	functionMap = parse_log(logname);
