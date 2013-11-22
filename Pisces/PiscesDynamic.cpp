aa9e047d82ca16c21065138e89ca0003aaca6500
vlouie <v.louie91@gmail.com>
2013-11-21 22:34:34 -0800

    Refactoring PiscesDynamic a bit

--- Pisces/PiscesDynamic.cpp
+++ Pisces/PiscesDynamic.cpp
@@ -13,9 +13,9 @@ using namespace std;
 int main(int argc, char* argv[]){
   string dirname = "../../fish-shell-master"; //path to codebase!!!!! TODO: update this? I'm leaving it like this for testing, otherwise you have to delete and re-copy it over every time due to the injection
   string logname = "log.txt"; 
-  string fishauto = "(cd ../../fish-shell-master && autoconf)"; // TODO: use dirname var instead?
-  string fishconf = "(cd ../../fish-shell-master && ./configure --prefix=$HOME)";
-  string fishmake = "make install -C ../../fish-shell-master";
+  string fishauto = "(cd " + dirname + " && autoconf)"; 
+  string fishconf = "(cd " + dirname + " && ./configure --prefix=$HOME)";
+  string fishmake = "make install -C " + dirname;
   string fishrun = "fish";
 
   exploreDirectory(dirname);
@@ -27,6 +27,7 @@ int main(int argc, char* argv[]){
   system(cmd2.c_str());
   system(fishrun.c_str());
   // Fish then opens and you have to give it input at this point
+  // use 'exit' to quit, still trying to figure this one out
 
 	unordered_map<string, int> functionMap;
 	functionMap = parse_log(logname);
